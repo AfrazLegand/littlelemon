cb2de9394cbc5d2c1da37f6fba61dbb286e31640
Afraz Siddiqui <0>
2024-08-11 18:02:28 +0100

    Add LittleLemon project folder

---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Afraz Siddiqui on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
